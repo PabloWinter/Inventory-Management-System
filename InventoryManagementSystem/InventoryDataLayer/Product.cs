bacc9299e3f5b05b16909173ea4b056e5e1feb4d
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-11-28 18:54:07 -0700

    Added example of queries to get information from database

---
+++ InventoryManagementSystem/InventoryDataLayer/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataLayer
+{
+    class Product
+    {
+
+        private void ShowProduct()
+        {
+            using (DataLinqToSQLDataContext products = new DataLinqToSQLDataContext())
+            {
+                // dataGridView1.DataSource = employee.GetTable<TOrderItem>();
+
+                var query = from product in products.TProductGroups
+                            select product;
+
+                IQueryable<TProductGroup> prodQuery =
+                                       from prod in products.TProductGroups
+                                       orderby prod.BrandID
+                                       select prod;
+
+                //var query = item.PR_Employees.OrderBy(x => x.FirstName).ThenByDescending(x => x.HourlyRate);
+
+                dataGridView1.DataSource = custQuery;
+
+            }
+
+        }
+    }
+}
