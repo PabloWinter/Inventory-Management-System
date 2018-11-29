3875c84c75b23a6730df5a7fc93ddd04d3eadbd9
a.resendeviana683 <a.resendeviana683@mybvc.ca>
2018-11-28 19:05:34 -0700

    Initial commit to branch. Search class created with some simple query examples.

---
+++ InventoryManagementSystem/InventoryDataLayer/Search.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataLayer
+{
+    class Search
+    {
+        DataLinqToSQLDataContext connect = new DataLinqToSQLDataContext();
+
+        public void BrandById(int value)
+        {
+            var test1 = connect.TBrands.Select(r => r.BrandID == value);
+
+            var test2 = from r in connect.TBrands
+                        where r.BrandID == value
+                        select r;
+
+            var test3 = connect.ExecuteQuery<TBrand>(   @"SELECT *
+                                                        FROM TBrand
+                                                        WHERE BrandID = {0}", value);
+
+
+        }
+    }
+}
